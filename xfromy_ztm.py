9202324cd5b7695ab38d16adfcf08e609aba119f
spackle-xmr <93713512+spackle-xmr@users.noreply.github.com>
2022-05-31 15:32:48 -0700

    Update xfromy_ztm.py

--- xfromy_ztm.py
+++ xfromy_ztm.py
@@ -1,13 +1,33 @@
-#match xfromy
-def xfromy_ztm(yin):
-
-    #biny = bin(yin)[2:].zfill(260) #weird experiment
-    biny = bin(yin)[2:].zfill(255)
+def xfrompoint_ztm(pointcheck): #Function receives Point, calculates x coordinate via 2.4.2
+    ypbytes=bytearray.fromhex(repr(pointcheck))
+    biny = bin(int.from_bytes(ypbytes, "little"))[2:].zfill(256)
     b = biny[0]
     newbin = biny[1:]
     newy = '0' + newbin
-    #y = int(newy,2)
-    y=yin #passing yprime to decoding process seemingly gives correct answer w/modified logic.
+    y = int(newy,2)
+    xp = 0
+
+    u = (y*y - 1) % q 
+    v = (d*y*y + 1) % q
+    z = (u * v**3) * pow(u*v**7,((q-5)//8), q) % q
+
+    if ((v*z**2) % q) == (u % q):
+        xp = (z % q)
+    if ((v*z**2) % q) == (-u % q):
+        xp = z * pow(2,((q-1)//4), q) % q
+    
+    if b != bin(xp)[-1]:
+        return -xp % q
+    else:
+        return xp % q
+
+
+def xfromy_ztm(yp): #Function receives integer y value, calculates x coordinate via 2.4.2
+    biny = bin(yp)[2:].zfill(256)
+    b = biny[0]
+    newbin = biny[1:]
+    newy = '0' + newbin
+    y = int(newy,2)
     xp = 0
 
     u = (y*y - 1) % q 
@@ -19,11 +39,11 @@ def xfromy_ztm(yin):
     if ((v*z**2) % q) == (-u % q):
         xp = z * pow(2,((q-1)//4), q) % q
 
-    print('b=',b)
-    print('xp_bit=',bin(xp)[-1])
-
-    #if b != bin(xp)[-1]:
-    if bin(xp)[-1] == '1':
+    print(b)
+    print(bin(xp)[-1])
+    
+    if b != bin(xp)[-1]:
         return -xp % q
     else:
         return xp % q
+
